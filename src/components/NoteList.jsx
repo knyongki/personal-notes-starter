d2cc567c9f413d21735b43af5f4e7eb1afb5ee79
knyongki <yyongkisetiawan@gmail.com>
2024-09-25 00:04:17 +0700

    kondision if notes in not found

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import NoteItem from "./NoteItem";
 
 function NoteList({ notes, onDelete, onArchive }) {
+  if (notes.length === 0) {
+    return <p className="notes-list__empty-message">Tidak ada catatan</p>;
+  }
+  
   return (
     <div className="notes-list">
       {notes.map((note) => (
